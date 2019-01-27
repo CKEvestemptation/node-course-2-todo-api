1d2e2077fef54c38fc4e85edfe9dd3a8b3455f6c
Renjun Liu Dell <renjun@evestemptation.com>
2019-01-26 21:34:26 -0800

    Add update & delete script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,27 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log(`Unable to connect to MongoDB server`);
+    }
+    console.log(`Connected to MongoDB server`);
+
+    db.collection('Users').findOneAndUpdate({
+        name: 'Jen'
+    },{
+        $set: {
+            name: 'CK'
+        },
+        $inc: {
+            age: 10
+        }
+    },{
+        returnOriginal: false
+    }).then((result)=>{
+        console.log(result);
+    });
+
+    db.close();
+});
