f2fcd5eb43164334541926fec6d0d8060ebe5b0f
Renjun Liu Dell <renjun@evestemptation.com>
2019-02-05 15:33:32 -0800

    Associate users with todos

--- server/server.js
+++ server/server.js
@@ -13,9 +13,10 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
     var todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator: req.user._id
     });
 
     todo.save().then((doc) => {
@@ -26,14 +27,17 @@ app.post('/todos', (req, res) => {
 });
 
 // GET /todos/12345
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id',authenticate, (req, res) => {
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
         console.log(`Id is invalid`);
         res.status(404).send();
     } else {
-        Todo.findById(id).then((todo) => {
+        Todo.findOne({
+            _id: id,
+            _creator: req.user._id
+        }).then((todo) => {
             if (!todo) {
                 res.status(404).send(`Id: ${id} is not found!`);
             } else {
@@ -47,8 +51,10 @@ app.get('/todos/:id', (req, res) => {
 });
 
 //GET /todos
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
+app.get('/todos',authenticate, (req, res) => {
+    Todo.find({
+        _creator: req.user._id
+    }).then((todos) => {
         res.send(todos)
     }).catch((e) => {
         res.status(400).send();
@@ -56,14 +62,17 @@ app.get('/todos', (req, res) => {
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id',authenticate, (req, res) => {
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
         console.log(`Id is invalid`);
         res.status(404).send();
     } else {
-        Todo.findByIdAndRemove(id).then((todo) => {
+        Todo.fineOneAndRemove({
+            _id: id,
+            _creator: req.user._id
+        }).then((todo) => {
             if (!todo) {
                 res.status(404).send(`Id: ${id} not found!`);
             } else {
@@ -78,7 +87,7 @@ app.delete('/todos/:id', (req, res) => {
 });
 
 // PATCH
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id',authenticate, (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']);
 
@@ -92,7 +101,10 @@ app.patch('/todos/:id', (req, res) => {
         body.completed = false;
         body.completedAt = null;
     }
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    Todo.fineOneAndUpdate({
+        _id: id,
+        _creator: req.user._id
+    }, {$set: body}, {new: true}).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
