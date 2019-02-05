a4b57f79532dcac3d16ac0d41c37ba881e3959d1
Renjun Liu Dell <renjun@evestemptation.com>
2019-02-05 15:12:18 -0800

    Add DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -124,18 +124,25 @@ app.get('/users/me', authenticate, (req, res) => {
 });
 
 // POST /users/login {email, password}
-app.post('/users/login',(req, res)=>{
+app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
 
-    User.findByCredentials(body.email, body.password).then((user)=>{
-        user.generateAuthToken().then((token)=>{
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
             res.header('x-auth', token).send(user);
         });
-    }).catch((e)=>{
+    }).catch((e) => {
         res.status(400).send();
     });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+});
 
 
 app.listen(port, () => {
