834008e9713e3f88a46f7982880810925e1c95f3
Renjun Liu Dell <renjun@evestemptation.com>
2019-02-05 14:51:03 -0800

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -51,6 +51,11 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+/**
+ *
+ * @param token
+ * @returns {*}
+ */
 UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
@@ -68,6 +73,32 @@ UserSchema.statics.findByToken = function (token) {
         '_id': decoded._id,
         'tokens.token': token,
         'tokens.access': 'auth'
+    });
+};
+
+/**
+ *
+ * @param email
+ * @param password
+ * @returns {Promise}
+ */
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
     });
 };
 
